bfc23d3d5e5b5f17f890edec7c0ad9470a7bc1c4
Daisuke Takeshita <daisuke.takeshita@helsinki.fi>
2019-10-31 11:32:27 +0900

    some changes

--- run_my_euler.m
+++ run_my_euler.m
@@ -1,14 +1,13 @@
 clear; close;
 %% use my_euler.m
-dydt = @(a)(@(y)a*y);
-a = -1;
+dydt = @(y)-2*y;
 y0 = 10;
 tspan = [0 4];
 h = 0.001;
-[t, y] = my_euler(dydt(a), tspan, y0, h);
+[t, y] = my_euler(dydt, tspan, y0, h);
 %% analytical solution
 t_analy = tspan(1):1e-4:tspan(2);
-y_analy = y0*exp(a*t_analy);
+y_analy = y0*exp(-2*t_analy);
 %% plot
 plot(t_analy,y_analy,'o')
 hold on
