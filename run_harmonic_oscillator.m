c294b74bde46acfbb77bfcd069bc0784be8c62e8
Daisuke Takeshita <daisuke.takeshita@helsinki.fi>
2019-11-11 10:45:10 +0900

    little change

--- run_harmonic_oscillator.m
+++ run_harmonic_oscillator.m
@@ -1,15 +1,18 @@
 clear; close all;
-tspan = [0 20];
-y0 = [1;0];
+tspan = [0 20];%time interval 
+x0 = [1;0];%initial condition
 m = 1; k = 1;
 f = @(t, x)harmonic_oscillator(t,x,m,k);
-[t,y] = ode45(f, tspan, y0);
+[t,y] = ode45(f, tspan, x0);
 %% plot position and velocity
 figure;
+subplot(1,2,1)
 plot(t,y(:,1))
 hold on
 plot(t,y(:,2))
 legend({'Position','Velocity'})
+xlabel('Time (sec)');ylabel('Position or velocity')
 %% plot Lissajous curve
-figure;
+subplot(1,2,2)
 plot(y(:,1),y(:,2))
+xlabel('X');ylabel('V');
