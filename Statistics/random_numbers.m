778658b35569a0c7c194700442f4abf54efbc7d5
Daisuke Takeshita <daisuke.takeshita@helsinki.fi>
2019-11-29 16:19:23 +0900

    statistics some update

--- Statistics/random_numbers.m
+++ Statistics/random_numbers.m
@@ -1,11 +1,48 @@
 %generation of random numbers
+
+%% uniform distribution
 clear
 close all
-%% uniform distribution
-x = rand(10,1);%a vector of random numbers from uniform distribution
+x = rand(10,1);%a vector of random numbers from uniform distribution [0,1]
 y = rand(3);%a matrix of random numbers from uniform distribution
-z = rand(10000,1);
-histogram(z)%draw a histogram
+z1 = rand(10000,1);
+z2 = rand(10000,1);
+z3 = rand(10000,1);
+z4 = rand(10000,1);
+z5 = z1 + z2;
+z6 = z1 + z2 + z3;
+z7 = z1 + z2 + z3 + z4;
+mean(z1) %(b-a)/2
+var(z1) %1/12*(b-a)^2
+subplot(2,2,1)
+histogram(z1)%draw a histogram
+title('random variable with uniform distribution')
+subplot(2,2,2)
+histogram(z5)
+title('sum of two random variables with uniform distribution')
+subplot(2,2,3)
+histogram(z6)
+title('sum of three random variables with uniform distribution')
+subplot(2,2,4)
+histogram(z7)
+title('sum of three random variables with uniform distribution')
+
+%% Nomral distribution (Gaussian distribution)
+close all
+v = randn(10000,1);%Standard normal distribution (mean:0, standard distribution 1)
+w = randn(10000,1);
+a = v + 2;
+b = 2*v;
+c = v + w;
+d = w + b;%sigma^2(d) = sigma^2(w) + sigma^2(b)
+e = v + b;
+mean(v)
+mean(a)
+mean(c)
+var(v)
+var(a)
+var(c)
+histogram(c)
 
 
 
