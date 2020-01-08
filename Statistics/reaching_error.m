c9e3e2d3016793bb084663820f8059b688c7df0d
Daisuke Takeshita <daisuke.takeshita@helsinki.fi>
2020-01-08 20:09:23 +0900

    reaching code minor change

--- Statistics/reaching_error.m
+++ Statistics/reaching_error.m
@@ -4,7 +4,8 @@ r2 = 0.5;
 theta1_eq = 7/32*pi;
 theta2_eq = 9/32*pi;
 sd = 1/32*pi;
-N = 100;
+N = 50;
+plot_range = [0 1];
 % create uncorrelated noise
 dtheta1 = sd*randn(N,1);
 dtheta2 = sd*randn(N,1);
@@ -17,10 +18,10 @@ xend_uncorr = arrayfun(@(i)reaching_plot(r1,r2,theta1(i),theta2_uncorr(i)),...
     1:N,'unif',0)
 xend_uncorr = cat(1,xend_uncorr{:});
 title('Uncorrelated case')
-set(gca,'xlim',[-1 1],'ylim',[-1 1]);
+set(gca,'xlim',plot_range,'ylim',plot_range);
 
 % correlated - synergetic case
-c = 0.95;
+c = 0.9;
 dtheta2_syn = -c*dtheta1 + sqrt(1-c^2)*dtheta2;
 theta2_syn = theta2_eq + dtheta2_syn;
 subplot(1,3,2)
@@ -28,7 +29,7 @@ xend_syn = arrayfun(@(i)reaching_plot(r1,r2,theta1(i),theta2_syn(i)),...
     1:N,'unif',0);
 xend_syn = cat(1,xend_syn{:});
 title('Synergetic case')
-set(gca,'xlim',[-1 1],'ylim',[-1 1]);
+set(gca,'xlim',plot_range,'ylim',plot_range);
 % correlated - non-synergetic case
 dtheta2_nonsyn = c*dtheta1 + sqrt(1-c^2)*dtheta2;
 theta2_nonsyn = theta2_eq + dtheta2_nonsyn;
@@ -37,5 +38,5 @@ xend_nonsyn =arrayfun(@(i)reaching_plot(r1,r2,theta1(i),theta2_nonsyn(i)),...
     1:N,'unif',0)
 xend_nonsyn = cat(1,xend_nonsyn{:});
 title('Non-synergetic case')
-set(gca,'xlim',[-1 1],'ylim',[-1 1]);
+set(gca,'xlim',plot_range,'ylim',plot_range);
 
