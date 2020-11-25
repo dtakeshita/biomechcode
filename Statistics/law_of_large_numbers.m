6b2b9cd873fa427d3625d3c2edc737afcc0e0c4d
Daisuke Takeshita <daisuke.takeshita@helsinki.fi>
2020-11-25 16:03:06 +0900

    law of large number finished

--- Statistics/law_of_large_numbers.m
+++ Statistics/law_of_large_numbers.m
@@ -4,8 +4,8 @@
 clear
 close all
 N = 100000;
-mu = 5;%mean
-sigma = 3;%standard deviation
+mu = 50;%mean
+sigma = 10;%standard deviation
 %% initialization
 mu_set = zeros(3,1);
 var_set = mu_set; 
@@ -34,6 +34,25 @@ h2 = histogram(y_ave,'Normalization','pdf', 'DisplayStyle', 'stairs', 'linewidth
 h3 = histogram(y_ave2,'Normalization','pdf', 'DisplayStyle', 'stairs', 'linewidth',lw);
 xlabel('Value'); ylabel('Probability density');
 legend([h h2 h3], {'No average','10 averages', '100 averages'})
+%% plot the standard deviation as a function of number of averages
+figure
+subplot(1,2,1)
+n_ave = [1 10 100];%number of averages
+sd = sqrt(v);%standard deviation
+x_vec = 1:0.1:100;
+sd_theory = sigma./sqrt(x_vec);
+
+plot(n_ave,sd,'o')%linear scale
+hold on
+plot(x_vec, sd_theory,'r')
+title('Linear scale')
+xlabel('Number of averages');ylabel('Standard deviation')
+set(gca,'fontsize',18)
+subplot(1,2,2)
+loglog(n_ave,sd)
+set(gca,'ylim',[1 100],'fontsize',18)
+title('Log scale')
+xlabel('Number of averages');ylabel('Standard deviation')
 
 
 
