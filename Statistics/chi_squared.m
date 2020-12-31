3bafb20c878f7912b23d019a83e52502c41ffb61
Daisuke Takeshita <daisuke.takeshita@helsinki.fi>
2020-12-31 10:33:33 +0900

    chi-squared comment added

--- Statistics/chi_squared.m
+++ Statistics/chi_squared.m
@@ -1,8 +1,9 @@
 clear; close all;
 NW = [17 8];
 W = [18 28];
-eNW = sum(NW)*[35 36]/71;
+%35 guarding and 36 penetrating trials - 71 trials 
+eNW = sum(NW)*[35 36]/71;%expected G & P trials for NW 
 chiNW = sum((NW-eNW).^2./eNW);
-eW = sum(W)*[35 36]/71;
+eW = sum(W)*[35 36]/71;%expected G & P trials for W 
 chiW = sum((W-eW).^2./eW); 
 chi = chiNW + chiW
